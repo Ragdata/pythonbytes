eb5b0aaaf56ec2996f13bc4d3db80d1f69961940
Ragdata <ragdata@users.noreply.github.com>
2025-07-04 04:36:58 +1000

    update pythonbytes

--- pythonbytes/__init__.py
+++ pythonbytes/__init__.py
@@ -1,3 +1,5 @@
+# pythonbytes/__init__.py
+
 __app_name__ = "pythonbytes"
 __version__ = "v0.1.0"
 
--- pythonbytes/config.py
+++ pythonbytes/config.py
@@ -9,6 +9,12 @@
 # Copyright:    Copyright © 2024 Redeyed Technologies
 ####################################################################
 
+import os
+
+from pathlib import Path
+
+
+
 SYMBOL_ERROR = "✘"
 SYMBOL_WARNING = "🛆"
 SYMBOL_INFO = "✚"
@@ -22,3 +28,22 @@ COLOR_INFO = "blue"
 COLOR_SUCCESS = "green"
 COLOR_TIP = "cyan"
 COLOR_IMPORTANT = "magenta"
+
+INSTALL_DIR = Path.home() / '.dotfiles'
+LOG_LEVEL = 'INFO'
+LOG_DIR = Path.home() / '.dotfiles' / 'logs'
+
+
+def loadEnv():
+	"""
+	Load environment variables from the env list.
+	"""
+
+	envfile = Path(os.environ.get('LOG_DIR', Path.home() / '.dotfiles' / 'logs')) / '.env'
+
+	if envfile.exists():
+		with open(envfile, 'r') as f:
+			for line in f:
+				if line.strip() and not line.startswith('#'):
+					key, value = line.strip().split('=', 1)
+					os.environ[key.strip()] = value.strip()
